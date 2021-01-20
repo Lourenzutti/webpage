2935a8b848509ba45d97b45af0adde3f0054cf34
Rodolfo Lourenzutti <rodolfo.lourenzutti@gmail.com>
2021-01-20 09:29:56 -0800

    Working on histogram

--- scripts/sampling-dist.js
+++ scripts/sampling-dist.js
@@ -32,14 +32,8 @@ btn.onclick = (e) => {
 /**********************************/
 /****  Creates the histogram   ****/
 /**********************************/
-height = 200;
-width = 400;
-const svg = d3.create("svg")
-    .attr("viewBox", [0, 0, width, height]);
 
-
-
-/* COllecting the data from the table */
+/* Collecting the data from the table */
 table = document.querySelectorAll("table")[1];
 sample_means = Array(1140);
 entries = table.querySelectorAll("td")
@@ -51,4 +45,55 @@ entries.forEach(
             sample_means[cont++] = parseFloat(entry.textContent);
         }
     }
-)
+);
+
+// set the dimensions and margins of the graph
+var margin = { top: 10, right: 30, bottom: 30, left: 40 },
+    width = 460 - margin.left - margin.right,
+    height = 280 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("main")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+// X axis: scale and draw:
+var x = d3.scaleLinear()
+    .domain([0, 90])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+    .range([0, 400]);
+
+svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+// set the parameters for the histogram
+var histogram = d3.bin(sample_means)
+    .value(d => d)
+    .domain(x.domain())  // then the domain of the graphic
+    .thresholds(x.ticks(20)); // then the numbers of bins
+
+// And apply this function to data to get the bins
+const bins = histogram(sample_means);
+
+// Y axis: scale and draw:
+var y = d3.scaleLinear()
+    .range([height, 0]);
+y.domain([0, d3.max(bins, d => d.length)]);   // d3.hist has to be called before the Y axis obviously
+svg.append("g")
+    .call(d3.axisLeft(y));
+
+// append the bar rectangles to the svg element
+svg.selectAll("rect")
+    .data(bins)
+    .enter()
+    .append("rect")
+    .attr("x", 1)
+    .attr("transform", function (d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+    .attr("width", function (d) { return x(d.x1) - x(d.x0) - 1; })
+    .attr("height", function (d) { return height - y(d.length); })
+    .style("fill", "#69b3a2")
+
