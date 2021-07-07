8114731e5fdfed9aa43ec87b97b7af30f8ec0a0d
Rodolfo Lourenzutti <rodolfo.lourenzutti@gmail.com>
2021-07-06 22:52:51 -0700

    A bunch of updates

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,3 +1,53 @@
+/******************************
+******** Page Loader  *********
+******************************/
+
+// load the page with the back button
+window.onpopstate = (event) => {
+    load_page(event.state.page);
+}
+
+/**
+ * Make the request for the page and 
+ * update the current html document.
+ * @param  {string} path the desired file
+ */
+function load_page(path) {
+    const page = path.split("#")[0];
+
+    fetch(page)
+        .then(response => {
+            if (response.ok) {
+                return response.text();
+            }
+            return response.statusText;
+        })
+        .then(text => {
+            const parser = new DOMParser();
+            const html = parser.parseFromString(text, "text/html");
+            document.querySelector('main').innerHTML = html.querySelector("main")?.innerHTML ? html.querySelector("main")?.innerHTML : text;
+        });
+}
+
+// add event listener to load the pages.
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('[data-page]').forEach(link => {
+        link.onclick = (event) => {
+            load_page(link.dataset.page);
+            history.pushState({ page: link.dataset.page }, "", link.dataset.page)
+            return false;
+        };
+    });
+});
+
+
+
+
+
+
+/******************************
+**** Exercises' controller ****
+******************************/
 const exercises = document.querySelectorAll(".box-exercise");
 
 exercises.forEach(exercise => {
@@ -24,3 +74,5 @@ exercises.forEach(exercise => {
     });
 
 });
+
+
